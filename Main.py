bf05cb8fb2e42bafcc56e454c6f66a464d89461c
Paige <tuo57622@temple.edu>
2025-03-20 09:42:26 -0400

    Added enemy png file and hp bar

--- Main.py
+++ Main.py
@@ -1,10 +1,25 @@
 import pygame
 import os #required for .exe creation
 import sys #required for .exe creation
+from pygame import(
+    image,
+)
+from os.path import abspath, dirname
+
+BASE_PATH = abspath(dirname(__file__))
+IMAGE_PATH = BASE_PATH + "/images/"
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+
+IMG_NAMES = [
+    "enemySample40x40",
+]
+IMAGES = {
+    name: image.load(IMAGE_PATH + "{}.png".format(name)).convert_alpha()
+    for name in IMG_NAMES
+}
 
 #Allows us to wrap the game into a .exe file
 def resource_path(relative_path):
@@ -37,11 +52,17 @@ class Enemy:
         self.range = range
         self.dmg = dmg
         self.cooldown = cooldown
+        self.x, self.y = 400, 200  # enemy position
+        self.image = IMAGES["enemySample40x40"]
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def draw(self):
-        pygame.draw.rect(screen, (255, 0, 0), (400, 200, 40, 40))  # Red square as a Enemy
+        #pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 40, 40))  # Red square as a Enemy
+        screen.blit(self.image, (self.x, self.y))
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y + 45, 40, 5))  # HP Bar Background White
+        hpPercentage = self.hp / 50 # Max HP: 50 = 100%
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y + 45, 40 * hpPercentage, 5))  # HP Bar Green
 
-# Create a tower object
 tower = Tower(200, 200, 100, 10, 2)
 enemy = Enemy(50, 10, 5, 3)
 
