909078cff040001f29e43cc3a10bd35518447242
tup00268 <tup00268@temple.edu>
2025-03-20 23:53:03 -0400

    added basic functionality for background music to play on launch

--- Main.py
+++ Main.py
@@ -10,6 +10,13 @@ BASE_PATH = abspath(dirname(__file__))
 IMAGE_PATH = BASE_PATH + "/images/"
 
 pygame.init()
+
+#code for background music
+pygame.mixer.init()
+pygame.mixer.music.load(BASE_PATH + "/sounds/backgroundmusic.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
 screen = pygame.display.set_mode((750, 400))
 clock = pygame.time.Clock()
 
