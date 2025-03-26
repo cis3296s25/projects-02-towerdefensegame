263741ead478dfdf770be24fc835587e15b278ea
tup00268 <tup00268@temple.edu>
2025-03-26 17:55:16 -0400

    added basic functionality for the first three waves. also added a wave helper function

--- Main.py
+++ Main.py
@@ -47,15 +47,31 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
+#wave helper function
+def get_wave_data(wave):
+    if wave == 1:
+        return["Red"] * 5
+    elif wave == 2:
+        return["Red"] * 10
+    elif wave == 3:
+        return["Red"] * 5 + ["Blue"] * 2
+    else:
+        return["Red"] * 5
+
 def game():
     mixer.music.play(-1) #plays music after leaving homescreen
 
     tower = Tower(160, 160, 100, 10, 2, screen, towerImage)
     # enemy = Enemy(WAYPOINTS[0][0], WAYPOINTS[0][1], 50, 10, 5, 3, screen)
+    
     enemies = []
-
     spawn_delay = 2500 #ms
     last_spawn_time = pygame.time.get_ticks()
+
+    #Wave Logic
+    wave_number = 3
+    current_wave_enemies = get_wave_data(wave_number) #what to spawn from current wave
+    spawned_count = 0         #how many have spawned from this wave
 
     gameMap = Map(screen, mapSample)
 
@@ -116,11 +132,19 @@ def game():
                     handle_rect.x = slider_rect.x + int(slider_rect.width * volume) - 5
         
         current_time = pygame.time.get_ticks()
-        if current_time - last_spawn_time >= spawn_delay:
-            color = random.choice(["Red", "Blue", "Purple"])
-            new_enemy = Enemy(WAYPOINTS[0][0], WAYPOINTS[0][1], 50, 10, 5, 3, screen, color)
-            enemies.append(new_enemy)
-            last_spawn_time = current_time
+        #if current_time - last_spawn_time >= spawn_delay:
+         #   color = random.choice(["Red", "Blue", "Purple"])
+          #  new_enemy = Enemy(WAYPOINTS[0][0], WAYPOINTS[0][1], 50, 10, 5, 3, screen, color)
+           # enemies.append(new_enemy)
+            #last_spawn_time = current_time
+        
+        if spawned_count < len(current_wave_enemies):
+            if current_time - last_spawn_time >= spawn_delay:
+                color = current_wave_enemies[spawned_count]
+                new_enemy = Enemy(WAYPOINTS[0][0], WAYPOINTS[0][1], 50, 10, 5, 3, screen, color)
+                enemies.append(new_enemy)
+                spawned_count += 1
+                last_spawn_time = current_time
 
         screen.fill((0, 0, 0))
         gameMap.draw()
