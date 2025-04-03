71837174cb2f180f07133f20615caa2b82c9e3f7
Tup88114 <157610239+Tup88114@users.noreply.github.com>
2025-04-03 11:54:59 -0400

    Made enemy data more readable
    made enemy data more readable

---
+++ EnemyData.py
@@ -0,0 +1,32 @@
+mob_data = {
+    "Red": {
+        "Damage": 1,
+        "Health": 50,
+        "Speed": 0.8,
+        "Money": 15
+    },
+    "Blue": {
+        "Damage": 3,
+        "Health": 30,
+        "Speed": 1.0,
+        "Money": 20
+    },
+    "Purple": {
+        "Damage": 5,
+        "Health": 70,
+        "Speed": 0.6,
+        "Money": 25
+    },
+    "Glowing": {
+        "Damage": 7,
+        "Health": 90,
+        "Speed": 0.4,
+        "Money": 30
+    },
+    "Giant": {
+        "Damage": 50,
+        "Health": 500,
+        "Speed": 0.1,
+        "Money": 250
+    }
+}
